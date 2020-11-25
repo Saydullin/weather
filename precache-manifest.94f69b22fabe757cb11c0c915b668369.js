93c4e3aaf02cf4a72f6dd73ebbe652caa6c991f9
Saydullin <66932545+Saydullin@users.noreply.github.com>
2020-11-25 19:21:05 +0500

    Add files via upload

---
+++ precache-manifest.94f69b22fabe757cb11c0c915b668369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02759e92357988a4d65a4e30f15c173a",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "f6b2fd04d635244c32e9",
+    "url": "/weather/static/css/main.bafaabed.chunk.css"
+  },
+  {
+    "revision": "17816ed45a3f4fe3dda1",
+    "url": "/weather/static/js/2.9e3b1a3f.chunk.js"
+  },
+  {
+    "revision": "4d0d56f2bee9e680eb9467d0c57060c8",
+    "url": "/weather/static/js/2.9e3b1a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b2fd04d635244c32e9",
+    "url": "/weather/static/js/main.bc418d86.chunk.js"
+  },
+  {
+    "revision": "6922094aca5ba85a5dc5",
+    "url": "/weather/static/js/runtime-main.24c2b153.js"
+  }
+]);
